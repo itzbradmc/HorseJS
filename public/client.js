6b1587d9c4d99b395244aa0dd6f1355f6b1f726d
Bradley WIlson <b.t.wilson@sky.com>
2019-01-05 21:44:09 +0000

    forms, ratings and simple prediction
    Added form and ratings processing and extremely basic winner predictions

--- public/client.js
+++ public/client.js
@@ -15,9 +15,17 @@ socket.on("progress", function showProgress(data){
 
 socket.on("cardsDone", function sendForms(){
   forms = JSON.parse(document.getElementById('forms_json').value);
-  socket.emit("forms", forms);
+  ratings = JSON.parse(document.getElementById('ratings_json').value);
+  socket.emit("formsRatings", {"forms": forms, "ratings": ratings});
 })
 
 socket.on("races", function showRaces(data){
   console.log(data)
+})
+
+socket.on("results", function showResults(data){
+  for (let i = 0; i < data.length; i++) {
+    document.getElementById('results').innerHTML += "<p> " + (i+1) + " - " +data[i].horse.cards.Horse+ " today: " + data[i].today + " total: " + data[i].total +"</p>"
+  }
+  document.getElementById('results').innerHTML += "<br>"
 })
--- server.js
+++ server.js
@@ -20,19 +20,21 @@ function newConnection(socket){
   console.log("New connection")
   races = []
 
-  socket.on("forms", processForms)
+  socket.on("formsRatings", processForms)
 
   socket.on("cards", processCards)
 
   function processForms(data){
-    formsRawData = data
+    var forms = data.forms
+    var ratings = data.ratings
+    formsRawData = forms
     console.log("forms")
     var tempRaces = []
     var race = {"time": "", "horses": [/*{form object}*/]}
     var lastTime = ""
-    console.log(data.length)
+    console.log(forms.length)
     for (let i = 0; i < cardsRawData.length; i++) {
-      var horse = data[i]
+      var horse = forms[i]
 
       if(i == 0){
         lastTime = horse.race_time
@@ -41,7 +43,7 @@ function newConnection(socket){
       if(horse.race_time != lastTime || i == cardsRawData.length-1){
         //console.log("start of a new race with time " + horse.RaceTime)
         tempRaces.push(race)
-        console.log("new race - " + horse.race_time + " " + horse.horse_name)
+        //console.log("new race - " + horse.race_time + " " + horse.horse_name)
         race = {"time":"", "horses": [/*{horse object}*/]}
         lastTime = horse.race_time
       } else{
@@ -52,16 +54,16 @@ function newConnection(socket){
 
       var progressData = {
         "horse" : horse.Horse,
-        "progress" : (i/data.length)*100
+        "progress" : (i/forms.length)*100
       }
       socket.emit("progress", progressData)
     }
     var count = 0
+    //adds the forms to the existing races and horses
     races.forEach(race => {
       race.horses.forEach(horse => {
           tempRaces.forEach(tempRaces => {
             tempRaces.horses.forEach(tempHorse => {
-              console.log(horse.cards.Horse + " - " + tempHorse.horse_name)
               if(horse.cards.Horse == tempHorse.horse_name){
                 horse.form = tempHorse
                 var progressData = {
@@ -75,8 +77,56 @@ function newConnection(socket){
       });
     });
 
-    socket.emit("races", tempRaces)
+    //adds the ratings to the existing races and horses
+    processRatings(ratings)
+  }
+
+  function processRatings(ratings){
+
+    var tempRaces = []
+    var race = {"time": "", "horses": [/*{form object}*/]}
+    var lastTime = ""
+    console.log(ratings.length)
+    for (let i = 0; i < ratings.length; i++) {
+      var horse = ratings[i]
+
+      if(i == 0){
+        lastTime = horse.time
+      }
+
+      if(horse.time != lastTime || i == ratings.length-1){
+        //console.log("start of a new race with time " + horse.RaceTime)
+        tempRaces.push(race)
+        //console.log("new race - " + horse.race_time + " " + horse.horse_name)
+        race = {"time":"", "horses": [/*{horse object}*/]}
+        lastTime = horse.time
+      } else{
+        //console.log("Adding " + horse.Horse + " to the race with time " + lastTime)
+        race.time = horse.time
+        race.horses.push(horse)
+      }
+
+      var progressData = {
+        "horse" : horse.horse_name,
+        "progress" : (i/ratings.length)*100
+      }
+      socket.emit("progress", progressData)
+    }
+
+    races.forEach(race => {
+      race.horses.forEach(horse => {
+          tempRaces.forEach(tempRaces => {
+            tempRaces.horses.forEach(tempHorse => {
+              if(horse.cards.Horse == tempHorse.horse_name){
+                horse.rating = tempHorse
+              }
+            });
+          });
+      });
+    });
+
     socket.emit("races", races)
+    predictWinners()
   }
 
   function processCards(data){
@@ -109,6 +159,33 @@ function newConnection(socket){
       socket.emit("progress", progressData)
     }
     socket.emit("cardsDone")
+  }
+
+  function predictWinners(){
+    races.forEach(race => {
+      var horses = race.horses
+      var todayTotal = getTodayAndTotal(horses)
+      todayTotal.sort(function(x, y) { // y = current x = last
+
+
+        if (x.today < y.today) {
+          return 1;
+        }
+        if (x.today > y.today || (y.total-x.total) > 25) {
+          return -1;
+        }
+        return 0;
+      });
+      socket.emit("results", todayTotal)
+    });
+  }
+
+  function getTodayAndTotal(horses){
+    var result = []
+    horses.forEach(horse => {
+      result.push({"today": horse.rating.today, "total": horse.rating.total, "horse": horse})
+    });
+    return result
   }
 
 }
